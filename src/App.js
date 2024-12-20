942ebbb24858e91d50fd54c4fc9f83f27421e985
ShahrukhNaqvi94 <sagi94@live.com>
2024-12-20 16:07:07 +0500

    Hire@E Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Profile from './components/Profile';
-import GetDate from './components/GetDate';
-import Image from './components/Image';
+
 
 function App() {
 
   return (
     <div className="App">
-      <h1>HEllo</h1>
-      <Profile name="Syed" type="premier"/>
-      <Profile name="Sana" type="normal"/>
-      <GetDate/>
-      <Image/>
+      <h1>hire@e</h1>
     </div>
   );
 }
